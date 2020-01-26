600d757ea9e028a27d482e91350af6f5006ded62
Benlio10 <leolucaspereiradasilva@gmail.com>
2020-01-26 06:31:11 +0000

    frontend page design

--- web/src/App.js
+++ web/src/App.js
@@ -1,45 +1,144 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
-  return (
-    <div id="app">
-      <aside>
-        <strong>Login</strong>
-        <form>
-            <div className="input-block">           
-                <label htmlFor="github_username">Utilizador do GitHub</label>
-                <input name="github_username" id="github_username" required />
-            </div> 
+    const [ github_username, setGithubUsername ] = useState("")
+    const [ techs, setTechs ] = useState("")
 
-            <div className="input-block"> 
-                <label htmlFor="techs">Tecnologias</label>
-                <input name="techs" id="techs" required />
-            </div> 
+    const [ latitude, setLatitude ] = useState("")
+    const [ longitude, setLongitude ] = useState("")
 
-            <div className="input-group">
-                <div className="input-block"> 
-                    <label htmlFor="latitude">Latitude</label>
-                    <input name="latitude" id="latitude" required />
-                </div> 
-                
-                <div className="input-block"> 
-                    <label htmlFor="longitude">Longitude</label>
-                    <input name="longitude" id="longitude" required />
-                </div> 
-            </div>  
-        </form>
+    useEffect( () => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const { latitude, longitude } = position.coords
 
-        <button type="submit">Salvar</button>
-      </aside>
-      <main>
+                setLatitude(latitude)
+                setLongitude(longitude)
+            }, (err) => {
+                console.log(err)
+            }, {
+                timeout: 30000
+            }
+        )
+    })
 
-      </main>
-    </div>
-  );
+    async function handleAddDev(e) {
+        e.preventDefault();
+    }
+
+    return (
+        <div id="app">
+            <aside>
+                <strong>Login</strong>
+                <form onSubmit={ handleAddDev }>
+                    <div className="input-block">           
+                        <label htmlFor="github_username">Utilizador do GitHub</label>
+                        <input 
+                            name="github_username" 
+                            id="github_username" 
+                            required
+                            value={github_username}
+                            onChange={e => setGithubUsername(e.target.value)}
+                        />
+                    </div> 
+
+                    <div className="input-block"> 
+                        <label htmlFor="techs">Tecnologias</label>
+                        <input
+                            name="techs" 
+                            id="techs" 
+                            required
+                            value={techs}
+                            onChange={e => setTechs(e.target.value)}
+                        />
+                    </div> 
+
+                    <div className="input-group">
+                        <div className="input-block"> 
+                            <label htmlFor="latitude">Latitude</label>
+                            <input 
+                                type="number"
+                                name="latitude" 
+                                id="latitude" 
+                                required 
+                                value={ latitude } 
+                                onChange={e => setLatitude(e.target.value)}
+                            />
+                        </div> 
+                        
+                        <div className="input-block"> 
+                            <label htmlFor="longitude">Longitude</label>
+                            <input 
+                                type="number" 
+                                name="longitude" 
+                                id="longitude" 
+                                required 
+                                value={ longitude } 
+                                onChange={e => setLatitude(e.target.value)}
+                            />
+                        </div> 
+                    </div>
+
+                    <button type="submit">Salvar</button>  
+                </form>
+            </aside>
+
+            <main>
+                <ul>
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars1.githubusercontent.com/u/2254731?s=400&v=4" alt="Diego Fernandes"/>
+                            <div className="user-info">
+                                <strong>Diego Fernandes</strong>
+                                <span>ReactJS, ReactNative, NodeJS</span>
+                            </div>
+                        </header>
+                        <p>just vibbin'</p>
+                        <a href="https://github.com/diego3g">Perfil no GitHub</a>
+                    </li>
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars1.githubusercontent.com/u/2254731?s=400&v=4" alt="Diego Fernandes"/>
+                            <div className="user-info">
+                                <strong>Diego Fernandes</strong>
+                                <span>ReactJS, ReactNative, NodeJS</span>
+                            </div>
+                        </header>
+                        <p>just vibbin'</p>
+                        <a href="https://github.com/diego3g">Perfil no GitHub</a>
+                    </li>
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars1.githubusercontent.com/u/2254731?s=400&v=4" alt="Diego Fernandes"/>
+                            <div className="user-info">
+                                <strong>Diego Fernandes</strong>
+                                <span>ReactJS, ReactNative, NodeJS</span>
+                            </div>
+                        </header>
+                        <p>just vibbin'</p>
+                        <a href="https://github.com/diego3g">Perfil no GitHub</a>
+                    </li>
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars1.githubusercontent.com/u/2254731?s=400&v=4" alt="Diego Fernandes"/>
+                            <div className="user-info">
+                                <strong>Diego Fernandes</strong>
+                                <span>ReactJS, ReactNative, NodeJS</span>
+                            </div>
+                        </header>
+                        <p>just vibbin'</p>
+                        <a href="https://github.com/diego3g">Perfil no GitHub</a>
+                    </li>
+                </ul>
+            </main>
+        </div>
+    );
 }
 
 export default App;
